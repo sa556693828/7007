cb590ac8e7a2e93e9f5ebc8ca66a5e911812ad65
sa556693828 <sa556693828@gmail.com>
2024-02-27 11:40:36 +0800

    Update Logo

--- src/components/AE/p5/HomePageBox.js
+++ src/components/AE/p5/HomePageBox.js
@@ -112,7 +112,7 @@ const HomePageBox = ({ enable }) => {
                         // p.textSize(36);
 
                         boxes.push(boxA);
-                        Matter.World.add(engine.world, boxA);
+                        Matter.World.add(engine.world, boxA); 會會
                     }
                 };
                 new p5(sketch);
